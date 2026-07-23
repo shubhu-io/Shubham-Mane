32e5dc04343bf296c4f34924d7d871d2af487c60
shubhu-io <shubhamane10@gmail.com>
2026-07-23 13:16:12 +0530

    feat: add Pinterest social link across Hero, Navbar, Contact; add Twitter icon to Contact; fix all icons consistency

--- src/config/site.ts
+++ src/config/site.ts
@@ -17,6 +17,7 @@ export const siteConfig = {
     github: 'https://github.com/shubhu-io',
     linkedin: 'https://linkedin.com/in/shubham-mane-dev',
     twitter: 'https://twitter.com/shubhu-io',
+    pinterest: 'https://in.pinterest.com/shubhvibessss/',
     email: 'shubhamane10@gmail.com',
   },
   github: {
